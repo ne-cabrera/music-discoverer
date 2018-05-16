d62c180a3c048f42864e9b32092fd4bfa729b871
necabrera <ne.cabrera@uniandes.edu.co>
2018-05-15 21:42:35 -0500

    Bug fix

--- imports/ui/components/Songs/SongsList.jsx
+++ imports/ui/components/Songs/SongsList.jsx
@@ -4,16 +4,35 @@ import * as d3 from "d3";
 
 export default class SongsList extends Component {
 
-  componentDidUpdate(){
-    var songs = d3.select(".songsContainer");
-      
-    var p = songs.selectAll(".playListContainer");
-    p.style("background-color","white");
-    p.on("click", function() {
+  componentDidUpdate(prevProps){
+    var diff= this.areDifferent(prevProps, this.props);
+    if(diff){
+      var songs = d3.select(".songsContainer");
+      var p = songs.selectAll(".playListContainer");
       p.style("background-color","white");
-      d3.select(this)
-        .style("background-color","#e9ebeb");
-    });
+      p.on("click", function() {
+        p.style("background-color","white");
+        d3.select(this)
+          .style("background-color","#e9ebeb");
+      });
+    }
+  }
+
+  areDifferent(obj1, obj2){
+    var arr1 = obj1.songs;
+    var arr2 = obj2.songs;
+    var dif = false;
+    if(arr1.length !== arr2.length){
+      dif = true;
+    }
+    else{
+      for(let i = 0; i < arr1.length && !dif; i++){
+        if(arr1[i].id !== arr2[i].id){
+          dif = true;
+        }
+      }
+    }
+    return dif;
   }
 
   render() {
